71b10ab0d51ef4536f53ff4d59a133be49c69ca0
Shivesh <mmshivesh@gmail.com>
2020-04-23 23:18:44 +0530

    Common location for Video names and encoders

--- src/common_functions.py
+++ src/common_functions.py
@@ -1,5 +1,15 @@
 from datetime import date, datetime, time, timedelta
 import os
+import sys
+import glob
+
+# VIDEO_NAME = "How\ to\ Move\ the\ Sun\ -\ Stellar\ Engines.mp4"
+VIDEO_NAME = "Hypothesis\ testing-II.mp4"
+ENCODER = 'h264_videotoolbox'
+
+# If there's at most `flexibility` unselected elements between selected indices, select all of them.
+FLEXIBILITY = 10
+TARGET_LENGTH = 600 # (only a guideline) Crop might be longer including the important pieces between subs.
 
 # This worker runs the ffmpeg job using an os.system call
 def worker_encode(file_name, start_time, end_time, output_file_name, encoder):
@@ -20,11 +30,20 @@ def assign_workers(pool, worker, encoder, ip_video, data):
 def get_clip_duration(clip_list):
 	for entry in clip_list:
 		end_time = time.fromisoformat(entry['clip_end'].replace(',','.'))
+		entry['clip_end'] = entry['clip_end'].replace(',','.')
 		start_time = time.fromisoformat(entry['clip_start'].replace(',','.'))
+		entry['clip_start'] = entry['clip_start'].replace(',','.')
 		difference = datetime.combine(date.min, end_time) - datetime.combine(date.min, start_time)
 		entry['duration'] = (datetime.combine(date.min, time.min) + difference).strftime('%H:%M:%S')
 		# total_duration += round(difference.seconds+difference.microseconds/1000000, 3)
 	return clip_list
+
+# Function to cleanup the intermediate folder
+def cleanup_intermediate():
+	if query_yesno('Clean up the intermediate folder?'):
+			files = glob.glob('../intermediate/*')
+			for file in files:
+				os.remove(file)
 
 ###
 #  This function floors start timestamp and ceils end timestamps
@@ -37,3 +56,25 @@ def round_durations(clip_list):
 		endtime = datetime.combine(date.min, end_time) + timedelta(seconds=1)
 		entry['clip_end'] = str(endtime.time())
 	return clip_list
+
+def query_yesno(question, default="yes"):
+	valid = {"yes": True, "y": True, "ye": True,
+			 "no": False, "n": False}
+	if default is None:
+		prompt = " [y/n] "
+	elif default == "yes":
+		prompt = " [Y/n] "
+	elif default == "no":
+		prompt = " [y/N] "
+	else:
+		raise ValueError("invalid default answer: '%s'" % default)
+
+	while True:
+		sys.stdout.write(question + prompt)
+		choice = input().lower()
+		if default is not None and choice == '':
+			return valid[default]
+		elif choice in valid:
+			return valid[choice]
+		else:
+			sys.stdout.write("Invalid input. Enter one of these: [yes/no/y/n]\n")
