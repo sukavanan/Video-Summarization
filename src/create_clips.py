2a713338b281473d25e4534acc3d2a43f8112198
Shivesh <mmshivesh@gmail.com>
2020-04-23 23:11:31 +0530

    Remove logger and move to common

--- src/create_clips.py
+++ src/create_clips.py
@@ -4,32 +4,45 @@ import time
 import multiprocessing as mp
 import atexit
 import time as t
-from common_functions import round_durations, get_clip_duration, assign_workers, worker_encode
+from common_functions import round_durations, assign_workers, worker_encode, get_clip_duration, cleanup_intermediate, VIDEO_NAME, ENCODER
 import logging
 
 start_time = t.time()
 
-encoder = 'h264_videotoolbox'
 
-logger = logging.Logger("GeneralLogger")
+# # This worker runs the ffmpeg job using an os.system call
+# def worker_encode(file_name, start_time, end_time, output_file_name):
+# 	# ffmpeg -i {input.mp4} -ss {start_time} -t {end_time} output.mp4
+# 	os.system(f"ffmpeg -nostats -loglevel 0 -i {file_name} -ss {start_time} -to {end_time} -b:v 750k -c:v {ENCODER} -c:a copy {output_file_name}")
+
+# # This function assigns the ffmpeg job to multiple workers
+# def assign_workers(pool, ip_video, data):
+# 	import concurrent.futures as cf
+# 	with cf.ProcessPoolExecutor(None) as pp:
+# 		for entry in data:
+# 			output_file_name = f"../intermediate/{entry['clip_no']}.mp4"
+# 			start_time = entry['clip_start']
+# 			end_time = entry['clip_end']
+# 			pp.submit(worker_encode, ip_video, start_time, end_time, output_file_name)
 
 def exit_handler():
-	logger.info("Creating concatendated video output...")
-	os.system('ffmpeg -f concat -i ../intermediate/videofiles.txt -c copy -fflags +genpts ../outputs/final.mp4')
-	logger.info(f"{round(t.time()-start_time, 0)} seconds using {encoder}")
+	print("Creating concatenated video output...")
+	os.system('ffmpeg -nostats -loglevel 0 -f concat -i ../intermediate/videofiles.txt -c copy -fflags +genpts ../outputs/final.mp4')
+	print(f"{round(t.time()-start_time, 0)} seconds using {ENCODER}")
+	cleanup_intermediate()
 
 atexit.register(exit_handler)
 
 
 if __name__ == "__main__":
 	# start_time = time.time()
-	input_video_path = '../inputs/How\ to\ Move\ the\ Sun\ -\ Stellar\ Engines.mp4'
-	logger.info("Loading clip data...")
+	input_video_path = os.path.join('../inputs/', VIDEO_NAME)
+	print("Loading clip data...")
 	with open('../intermediate/clip_data.json', 'r') as f:
 		clip_data = json.load(f)
-	clip_data = round_durations(clip_data)
+	clip_data = round_durations(get_clip_duration(clip_data))
 	with mp.Pool(None) as pool:
-		assign_workers(pool, worker_encode, encoder, input_video_path, clip_data)
+		assign_workers(pool, worker_encode, ENCODER, input_video_path, clip_data)
 	
 	# Create videofiles.txt to enable final video combination
 	with open('../intermediate/videofiles.txt', 'w') as f:
